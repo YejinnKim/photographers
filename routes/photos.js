245e1d6abb3ebee81a34b6d23790f1524d52ec91
qjarl5678 <qjarl5678@nate.com>
2022-06-04 17:23:18 +0900

    feat/글쓰기 페이지 추가, 이미지업로드 억까당해버림 ;;

--- routes/photos.js
+++ routes/photos.js
@@ -1,6 +1,11 @@
 const express = require('express')
 const router = express.Router()
 const conn = require('./database')
+const multer = require('multer')
+const upload = multer({
+    dest: __dirname+'/uploads/',
+})
+
 
 router.get('/', (req, res) => {
     var sql = 'SELECT * FRMO contents'
@@ -9,7 +14,7 @@ router.get('/', (req, res) => {
     })
 })
 
-router.get('/:idx', async (req, res) => {
+router.get('/content/:idx', async (req, res) => {
     /*var idx = req.params.idx
     var sql = 'select * from contents where co_idx = ?'
     conn.query(sql, idx, (err, result) => {
@@ -17,5 +22,31 @@ router.get('/:idx', async (req, res) => {
     })*/
     res.render('photos_details')
 })
+
+
+router.get('/write', (req,res) => {
+    res.render('photo_write');
+})
+
+router.post('/write/imageUpload/', upload.array('uploadFile'), (req, res, next) => {
+    var data  = req.body;
+    console.log("body 데이터 : ", data);
+
+    //배열 형태이기 때문에 반복문을 통해 파일 정보를 알아낸다.
+    req.files.map(data => {
+      console.log("폼에 정의된 필드명 : ", data.fieldname);
+      console.log("사용자가 업로드한 파일 명 : ", data.originalname);
+      console.log("파일의 엔코딩 타입 : ", data.encoding);
+      console.log("파일의 Mime 타입 : ", data.mimetype);
+      console.log("파일이 저장된 폴더 : ", data.destination);
+      console.log("destinatin에 저장된 파일 명 : ", data.filename);
+      console.log("업로드된 파일의 전체 경로 ", data.path);
+      console.log("파일의 바이트(byte 사이즈)", data.size);
+    })
+    res.json({ok: true, data: "Multipart Upload Ok"})
+})
+
+
+
 
 module.exports = router
