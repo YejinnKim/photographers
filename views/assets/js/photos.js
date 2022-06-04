540bd6f2af37c61e25aba8d22914c7e6beb25ed4
qjarl5678 <qjarl5678@nate.com>
2022-06-04 19:09:09 +0900

    feat/다중파일 비동기 업로드 구현, express-session 추가

--- views/assets/js/photos.js
+++ views/assets/js/photos.js
@@ -1,25 +1,28 @@
 
 function imageUpload(){
-    const formData = new FormData();
-    const photos = document.querySelector('#uploadFile');
+    const uploadFile = new FormData();
+    // const photos = document.querySelector('#uploadFile');
     var imgCode = new Date().getTime().toString(36);
+    const photos = document.querySelector('input[type="file"][multiple]');
+   
+    // uploadFile.append(`uploadFile`, photos.files);
 
-    // console.log(imgCode);
-
-    formData.append('imageCode', imgCode);
     for (let i = 0; i < photos.files.length; i++) {
-        // console.log(photos.files[i]);
-        formData.append(`photos_` + i, photos.files[i]);
+        uploadFile.append('uploadFile', photos.files[i])
     }
 
+    uploadFile.append('imageCode', imgCode);
+
+    console.log(uploadFile);
+    // console.log(uploadFile);
     // FormData의 값 확인  
-    for (var value of formData.values()) {
-        console.log(value);
-    }
+    // for (var value of uploadFile.values()) {
+    //     console.log(value);
+    // }
 
     fetch('/photos/write/imageUpload', {
     method: 'POST',
-    body: formData,
+    body: uploadFile,
     })
     .then((response) => response.json())
     .then((result) => {
