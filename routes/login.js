503d54addd4d19de4874806c3b0207d99dd16247
qjarl5678 <qjarl5678@nate.com>
2022-06-06 16:54:55 +0900

    feat/로그인 에러처리 수정

--- routes/login.js
+++ routes/login.js
@@ -18,7 +18,7 @@ router.post('/:type', (req, res) => {
     var user = req.body
     console.log(req.params.type);
     if(req.params.type === 'login'){
-        var sql = `SELECT * FROM user WHERE user_id = '${user.user_id}' and user_pw = '${user.user_pw}'`
+        var sql = `SELECT * FROM user WHERE user_id = '${user.user_id}' and user_pw = '${user.user_pw}' limit 1`
         connection.query(sql, (error, results, fields) => {
             if (error) throw error;
             if(results[0]) {
@@ -26,8 +26,10 @@ router.post('/:type', (req, res) => {
                     id: user.user_id,
                     pw: user.user_pw
                 }
+                res.redirect('/')
+            }else{
+                res.status(400).send({message: "존재하지 않는 아이디입니다."});
             }
-            res.redirect('/')
         })
     }else if(req.params.type === 'signup'){
         var sql = ` INSERT INTO user ( user_id, user_pw, user_name, user_email ) VALUES ( '${user.user_id}', '${user.user_pw}', '${user.user_name}', '${user.user_email}')`;
