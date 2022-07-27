3008d63ad75e44f7531a13af42f32141188d1d2c
devBamki <54662917+qjarl5678@users.noreply.github.com>
2022-07-27 10:15:09 +0900

    Update database.js

--- routes/database.js
+++ routes/database.js
@@ -3,10 +3,10 @@ const mysql = require('mysql')
 
 // db 서버와 연결
 var connection = mysql.createConnection({
-    host: '121.254.153.149',
-    user: 'photographers',
+    host: '********',
+    user: '********',
     database: 'photographers',
-    password: 'photo12!@',
+    password: '********',
     port: '3306'
 })
 
